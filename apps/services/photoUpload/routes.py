99cdc87a53bf1ff24f77e7ba34a57fbdfb7a2d53
naqibfaiyaz <naqib3110@gmail.com>
2023-02-03 00:25:52 -0500

    commit the FE for assignment1

---
+++ apps/services/photoUpload/routes.py
@@ -0,0 +1,88 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from apps.services.photoUpload import blueprint
+from flask import render_template, request, redirect, url_for
+import requests
+from jinja2 import TemplateNotFound
+from apps import logger, api_endpoint
+from apps.services.home.routes import get_segment
+
+@blueprint.route('/index')
+# @login_required
+def index():
+
+    return render_template('home/index.html', segment='index')
+
+
+@blueprint.route('/<template>')
+# @login_required
+def route_template(template):
+    try:
+
+        if not template.endswith('.html'):
+            template += '.html'
+
+        # Detect the current page
+        segment = get_segment(request)
+
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        if segment=='photos.html':
+            return render_template("photoUpload/photos.html", memcache=getAllPhotos())
+        return render_template("photoUpload/" + template, segment=segment.replace('.html', ''))
+
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except Exception as e:
+        logger.error(str(e))
+        return render_template('home/page-500.html'), 500
+
+
+@blueprint.route('/put',methods=['POST', 'PUT'])
+def putPhoto():
+    # UPLOAD_FOLDER = apps.app_c'/static/assets/public/'
+    # ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+    if request.form.get('key') and request.files['image']:
+        response = requests.post(api_endpoint + '/upload', files={"file": request.files['image']}, data={"key": request.form.get('key')}).json()
+        
+        logger.info('Put request received- ' + str(response))
+
+        return render_template("photoUpload/photos.html", msg=response["msg"], memcache=getAllPhotos())
+    elif request.form.get('key'):
+        key = request.form.get('key')
+        cacheData = requests.post(api_endpoint + '/key/' + key, data={"key": key}).json()
+        if "content" in cacheData:
+            return render_template("photoUpload/addPhoto.html", msg="Key exists, please upload a new image", data=cacheData["content"], key=key)
+        elif key not in cacheData and "error" in cacheData:
+            return render_template("photoUpload/addPhoto.html", msg="Key/Image mismatch, please upload properly")
+    else:
+            return render_template("photoUpload/addPhoto.html", msg="Key/Image mismatch, please upload properly")
+    
+
+# @blueprint.route('/get', defaults={'url_key': None}, methods=['POST'])
+@blueprint.route('/get/<url_key>',methods=['GET'])
+def getSinglePhoto(url_key):
+    key = url_key or request.form.get('key')
+    logger.info(request.form)
+    cacheData = requests.post(api_endpoint + '/key/' + key, data={"key": key}).json()
+    logger.info('Get request received for single key- ' + key, str(cacheData))
+    logger.info(cacheData)
+    logger.info(request.method)
+    if "content" in cacheData:
+        return render_template("photoUpload/addPhoto.html", data=cacheData["content"], key=key)
+    elif "content" not in cacheData and "error" in cacheData:
+        return render_template("photoUpload/addPhoto.html", msg=cacheData["error"]["message"], key=key)
+
+@blueprint.route('/getAll',methods=['POST'])
+def getAllPhotos():
+    return requests.post(api_endpoint + '/list_keys').json()["content"]
+
+@blueprint.route('/invalidate_key/<url_key>',methods=['GET', 'POST'])
+def invalidateKey(url_key) :
+    response = requests.post(api_endpoint + '/invalidate/' + url_key, data={"key": url_key})
+    logger.info("invalidateKey response: " + str(response))
+    
+    return redirect(url_for("photoUpload_blueprint.route_template", template="photos.html"))
