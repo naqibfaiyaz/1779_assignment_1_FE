8a128eaeb00b1f927b017ad62734369c2e6f40a1
naqibfaiyaz <naqib3110@gmail.com>
2023-03-12 15:14:23 -0400

    autoscaler debug

--- apps/services/autoScaler/routes.py
+++ apps/services/autoScaler/routes.py
@@ -9,11 +9,9 @@ import boto3
 def autoScalerFunction():
     # Let's use Amazon S3
     missRate = requests.post(backendUrl + '/getRate?rate=miss').json()['value']
-    config = requests.post(backendUrl + '/getCurrentConfig').json()
+    config = requests.post(policyManagementUrl + '/getCurrentConfig').json()
     activeNodeCount = requests.post(backendUrl + '/getNumNodes').json()
-    print(missRate)
-    print(config)
-    print(activeNodeCount)
+
     # missRate=1
 
     if missRate > config['maxMiss']:
